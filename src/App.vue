798760e5aba1d97dc8185ba521aac7cb0026db94
waseemusman17 <waseem.usman17@gmail.com>
2023-01-23 02:52:29 +0500

    Basic Project Setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div>
+    <h1 class="text-3xl bg-blue-500 font-bold">
+      Hello world!
+    </h1>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="">
+  
+</style>
